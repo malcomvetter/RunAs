1d22ed9e8580deea9abced9192c7b6c752232e8b
malcomvetter <malcomvetter@gmail.com>
2018-08-12 22:52:50 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# RunAs
+
+Build in Visual Studio (.net 3.5 so it runs on Win 7). 
+
+This is a proof of concept, example project that shows a pattern for how to programmatically invoke UAC to escalate privileges of the current process to administrator in C#.
---
+++ RunAs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Security;
+using System.Net;
+
+namespace RunAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunThisAsAdmin();
+            Console.WriteLine("Running as admin!");
+            Console.ReadKey();
+        }
+
+        private static void RunThisAsAdmin()
+        {
+            if (!IsAdministrator())
+            {
+                var exe = Process.GetCurrentProcess().MainModule.FileName;
+                var startInfo = new ProcessStartInfo(exe)
+                {
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    CreateNoWindow = false
+                };
+                Process.Start(startInfo);
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+        private static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
